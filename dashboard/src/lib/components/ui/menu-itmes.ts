7cdb44634eb86d75988058116ea4da07c4a02d8d
Ctw <ctw@ctwhome.com>
2025-07-08 02:29:40 +0200

    feat: Remove validation table route and update menu items

--- dashboard/src/lib/components/ui/menu-itmes.ts
+++ dashboard/src/lib/components/ui/menu-itmes.ts
@@ -1,7 +1,6 @@
 export default [
 
 	{ title: 'About', path: '/about', displayTitle: 'About' },
-	{ title: 'Validation Table ', path: '/table', displayTitle: 'Input metrics Table (test)' },
 
 	// {
 	// 	title: 'Latest Work',
